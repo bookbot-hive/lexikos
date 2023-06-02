4ac65f4d5c88c919bb34593f1e18d2075a186492
Wilson Wongso <wilsonwong961@gmail.com>
2023-06-02 09:02:19 +0000

    Added ONNX Quantization Script

---
+++ examples/run_quantize_onnx.sh
@@ -0,0 +1,4 @@
+python3 quantize_onnx.py \
+    --model_name bookbot/onnx-byt5-small-wikipron-eng-latn-uk-broad \
+    --hub_model_id bookbot/onnx-byt5-small-wikipron-eng-latn-uk-broad-quantized-arm64 \
+    --architecture arm64
